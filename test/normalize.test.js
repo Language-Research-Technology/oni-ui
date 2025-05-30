82be299afb5b7dafb7af1b3dba633e973a7d6543
Moises Sacal <moisbo@gmail.com>
2025-05-30 11:40:22 +1000

    fixed normalizer

--- test/normalize.test.js
+++ test/normalize.test.js
@@ -1,7 +1,7 @@
 // test/normalize.test.js
 const assert = require('assert');
 const { ROCrate } = require('ro-crate');
-const { normalizeLdac } = require('../lib/normalize.js');
+const { normalizeLdac, getContext } = require('../lib/normalize.js');
 
 const ldacIri = 'https://w3id.org/ldac/terms#';
 
@@ -9,14 +9,29 @@ const ldacIri = 'https://w3id.org/ldac/terms#';
 describe('normalize.js',  () => {
   it('Should normalize context',  async() => {
     const crate = new ROCrate();
-    await crate.importContext('https://w3id.org/ldac/context')
+    // await crate.importContext('https://w3id.org/ldac/context')
     crate.addContext({"ldac": ldacIri});
     crate.addValues(crate.root, 'subjectLanguage', {"@id":"https://glottolog.org/resource/languoid/id/stan1293"});
-    normalizeLdac(crate);
+    const context = await getContext('https://w3id.org/ldac/context');
+    normalizeLdac(crate, context);
     assert.deepStrictEqual(
       crate.root['ldac:subjectLanguage'],
       { "@id": "https://glottolog.org/resource/languoid/id/stan1293" },
       'normalized subjectLanguage to ldac:subjectLanguage'
+    );
+  });
+
+  it('Should not normalize context',  async() => {
+    const crate = new ROCrate();
+    const context = await getContext('https://w3id.org/ldac/context');
+    // await crate.importContext('https://w3id.org/ldac/context')
+    crate.addContext({"ldac": ldacIri});
+    crate.addValues(crate.root, 'datePublished', "2025");
+    normalizeLdac(crate, context);
+    assert.deepStrictEqual(
+      crate.root['ldac:datePublished'],
+      undefined,
+      'not normalizing to datePublished'
     );
   });
 });

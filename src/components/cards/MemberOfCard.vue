5220868053a6c95b9c9391f42c26334e89d541b8
rosanna-smith <rosanna.smith@uq.edu.au>
2025-10-14 16:25:36 +1100

    fix: tooltips added for Member Of, Citation, creditText doesn't show in widget

--- src/components/cards/MemberOfCard.vue
+++ src/components/cards/MemberOfCard.vue
@@ -9,7 +9,11 @@ const link = `/${routePath}?id=${encodeURIComponent(memberOf['@id'])}`;
 
 <template>
   <el-card :body-style="{ padding: '0px' }" class="mx-10 p-5">
-    <h5 class="text-2xl font-medium ">Member Of</h5>
+    <h5 class="text-2xl font-medium">Member Of
+      <el-tooltip class="box-item" effect="light" trigger="hover" content="The collection or sub-collection the current item is a member of." placement="top">
+        <font-awesome-icon icon="fa-solid fa-circle-info" class="ml-2 cursor-pointer" size="xs" color="gray"/>
+      </el-tooltip>
+    </h5>
     <hr class="divider divider-gray pt-2" />
     <ul>
       <li class="font-semibold">

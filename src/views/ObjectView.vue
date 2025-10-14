4cfe1f2e4f7570b7b52864b39f9d19a4afc8e499
rosanna-smith <rosanna.smith@uq.edu.au>
2025-10-14 16:18:00 +1100

    fix: tooltip added for Access

--- src/views/ObjectView.vue
+++ src/views/ObjectView.vue
@@ -173,7 +173,11 @@ fetchdata();
       <el-row :gutter="20" :align="'middle'" class="justify-center content-center pb-5">
         <el-col v-if="metadata.license">
           <el-card :body-style="{ padding: '0px' }" class="mx-10 p-5">
-            <h5 class="text-2xl font-medium">Access</h5>
+            <h5 class="text-2xl font-medium">Access
+            <el-tooltip class="box-item" effect="light" trigger="hover" content="License and access conditions for the current object." placement="top">
+              <font-awesome-icon icon="fa-solid fa-circle-info" class="ml-2 cursor-pointer" size="xs" color="gray"/>
+            </el-tooltip>
+            </h5>
             <hr class="divider divider-gray pt-2" />
             <LicenseCard :license="metadata.license" />
           </el-card>

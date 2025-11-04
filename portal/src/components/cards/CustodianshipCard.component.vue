3c7cc81171778839d4f309f10568b5dd6224189f
Moises Sacal <moisbo@gmail.com>
2025-11-04 15:24:03 +1000

    fix undefined custodian

--- portal/src/components/cards/CustodianshipCard.component.vue
+++ portal/src/components/cards/CustodianshipCard.component.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card :body-style="{ padding: '0px' }" class="mx-10 p-5">
+  <el-card :body-style="{ padding: '0px' }" class="mx-10 p-5" v-if="rightsHolder && accountablePerson">
     <h5 class="text-2xl font-medium">Custodianship
       <el-tooltip class="box-item" effect="light" trigger="hover"
         content="The individual(s) or organisation(s) with the authority to make decisions regarding the collection." placement="top">
@@ -8,8 +8,8 @@
     </h5>
     <hr class="divider divider-gray pt-2" />
     <h4 class="text-1xl font-medium">
-      Accountable Person: {{ accountablePerson }}<br>
-      Rights Holder: {{ rightsHolder }}
+      <p v-if="accountablePerson">Accountable Person: {{ accountablePerson }}</p>
+      <p v-if="rightsHolder">Rights Holder: {{ rightsHolder }}</p>
     </h4>
   </el-card>
 </template>
@@ -30,7 +30,7 @@ export default {
       if (Array.isArray(this.accountablePerson) && this.accountablePerson.length > 0) {
         accountablePerson = this.accountablePerson.map(a => a?.name?.[0]?.['@value'] || a?.['@value']).filter(Boolean).join(', ')
       } else {
-        accountablePerson = 'undefined';
+        accountablePerson = undefined;
       }
       return accountablePerson;
     },
@@ -39,7 +39,7 @@ export default {
       if (Array.isArray(this['dct:rightsHolder']) && this['dct:rightsHolder'].length > 0) {
         rightsHolder = `${this['dct:rightsHolder'].map(a => a?.name?.[0]?.['@value'] || a?.['@value']).filter(Boolean).join(', ')}`;
       } else {
-        rightsHolder = 'undefined';
+        rightsHolder = undefined;
       }
       return rightsHolder;
     },

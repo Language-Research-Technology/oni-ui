eb6e25b3f9734e5110022b866e55dfe3d44a70b8
John Ferlito <johnf@inodes.org>
2025-12-04 08:12:09 +1100

    feat: remove ldaca proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,10 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
-// DOCKER_REMOVE_START
-import { ldacaProxy } from './ldaca-proxy';
-// DOCKER_REMOVE_END
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    // DOCKER_REMOVE_START
-    {
-      // NOTE: Temp so we can transform the old LDaCA API
-      name: 'ldaca-proxy',
-      configureServer(server) {
-        server.middlewares.use(ldacaProxy);
-      },
-    },
-    // DOCKER_REMOVE_END
-    mode === 'production' ? sentryVitePlugin() : undefined,
-  ],
+  plugins: [vue(), vueDevTools(), mode === 'production' ? sentryVitePlugin() : undefined],
 
   resolve: {
     alias: {
